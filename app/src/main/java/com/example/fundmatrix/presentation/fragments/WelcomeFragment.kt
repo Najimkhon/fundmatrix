ee787c7d89899522f9931dca3ed4e8f188199d9c
Najimkhon <qwerty1995.290@gmail.com>
2023-06-23 20:27:08 +0500

    Setting up base class, navgraph and WelcomeFragment

---
+++ app/src/main/java/com/example/fundmatrix/presentation/fragments/WelcomeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.fundmatrix.presentation.fragments
+
+import com.example.fundmatrix.databinding.FragmentWelcomeBinding
+import com.example.fundmatrix.presentation.base.BaseFragment
+
+class WelcomeFragment : BaseFragment<FragmentWelcomeBinding>(FragmentWelcomeBinding::inflate) {
+
+}
